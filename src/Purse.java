4818d1dbb65fbcd9c3bc332fd8f2350e0fe735b9
jumorg <justincodeemail@gmail.com>
2024-09-01 17:46:53 -0500

    Initial commit

---
+++ src/Purse.java
@@ -0,0 +1,127 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Purse {
+
+    // A record that will hold the denominations
+    public record Denomination(String name, double amount, Form form, String img) {
+        // Defined Enum for Form.
+        public enum Form {
+            BILL, COIN
+        }
+    }
+
+    // Define bill denominations
+    private final Denomination hundredBill = new Denomination("Hundred Dollar Bill", 100.00, Denomination.Form.BILL, "hundred.png");
+    private final Denomination fiftyBill = new Denomination("Fifty Dollar Bill", 50.00, Denomination.Form.BILL, "fifty.png");
+    private final Denomination twentyBill = new Denomination("Twenty Dollar Bill", 20.00, Denomination.Form.BILL, "twenty.png");
+    private final Denomination tenBill = new Denomination("Ten Dollar Bill", 10.00, Denomination.Form.BILL, "ten.png");
+    private final Denomination fiveBill = new Denomination("Five Dollar Bill", 5.00, Denomination.Form.BILL, "five.png");
+    private final Denomination oneBill = new Denomination("One Dollar Bill", 1.00, Denomination.Form.BILL, "one.png");
+
+    // Define coin denominations
+    private final Denomination quarterCoin = new Denomination("Quarter", 0.25, Denomination.Form.COIN, "quarter.png");
+    private final Denomination dimeCoin = new Denomination("Dime", 0.10, Denomination.Form.COIN, "dime.png");
+    private final Denomination nickelCoin = new Denomination("Nickel", 0.05, Denomination.Form.COIN, "nickel.png");
+    private final Denomination pennyCoin = new Denomination("Penny", 0.01, Denomination.Form.COIN, "penny.png");
+
+    // HashMap that will represent the money in a purse
+    private final HashMap<Denomination, Integer> cash = new HashMap<>();
+
+    // Method that adds a number of particular denominations to cash HashMap
+    public void add(Denomination d, int amount) {
+        if (cash.containsKey(d)) {
+            cash.put(d, cash.get(d) + amount); // If denomination exist add x amount
+        } else {
+            cash.put(d, amount); // If denomination does not exist than add starting amount
+        }
+    }
+
+    // Removes an amount of a certain denomination and returns the amount left in purse
+    public double remove(Denomination d, int amount) {
+        // Finds the denomination and removes x amount of them
+        if (cash.containsKey(d)) {
+            cash.put(d, cash.get(d) - amount);
+        }
+
+        // Returns a double of the total amount left in the purse
+        return getValue();
+    }
+
+    // Returns the total amount of money in the purse
+    public double getValue() {
+        double totalAmount = 0.0;
+
+        // Cycles through the cash HashMap and generates the total amount of money
+        for (HashMap.Entry<Denomination, Integer> entry : cash.entrySet()) {
+            Denomination d = entry.getKey();
+            int quantity = entry.getValue();
+
+            // Calculate value of denomination and add to total
+            totalAmount += d.amount * quantity;
+
+        }
+        return totalAmount;
+    }
+
+    // Returns a string representation of the Purse and its contents
+    public String toString() {
+
+        // Initialize new StringBuilder
+        StringBuilder sb = new StringBuilder();
+
+        // Stream the entries of the cash map, sort them by Denomination amount in descending order
+        cash.entrySet().stream()
+                .sorted((entry1, entry2) -> Double.compare(entry2.getKey().amount(), entry1.getKey().amount()))
+                .forEach(entry -> {
+                    Denomination d = entry.getKey();
+                    String quantity = Integer.toString(entry.getValue()); // Converts int quantity to string
+
+                    // Append sorted entries to the StringBuilder
+                    sb.append(quantity).append(" ").append(d.name()).append(" ").append("\n");
+                });
+
+        return sb.toString();
+    }
+
+    // Store all denominations in a list for easy iteration over
+    private final List<Denomination> allDenomination = List.of(
+            hundredBill, fiftyBill, twentyBill, tenBill, fiveBill, oneBill,
+            quarterCoin, dimeCoin, nickelCoin, pennyCoin
+    );
+
+    // Returns all denominations
+    public List<Denomination> getAllDenomination() {
+        return allDenomination;
+    }
+}
+
+//public static void main(String[] args) {
+//////        // Create a new Purse instance
+//  Purse purse = new Purse();
+//////
+//////        // Add some denominations to the purse
+// purse.add(purse.hundredBill, 2);  // Add 2 hundred dollar bills
+// purse.add(purse.fiftyBill, 1);    // Add 1 fifty dollar bill
+//purse.add(purse.oneBill, 4);  // Add 4 quarters
+//    purse.add(purse.quarterCoin, 4);  // Add 4 quarters
+//
+//////
+//////        // Calculate and print the total value
+//////        double totalValue = purse.getValue();
+//////
+//////        System.out.println(totalValue);
+//////
+//////        // Testing removal
+//////        // double re = purse.remove(purse.hundredBill, 1);
+//////
+//////        //totalValue = purse.getValue();
+//////
+//////        //System.out.println(totalValue);
+//String totalStringValue = purse.toString();
+//System.out.println(purse.toString());
+//////
+//}
+////
+//}
---
+++ src/Register.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Register {
+
+    // Takes an amount and returns a Purse containing that amount in the fewest number of bills and coins
+    public static Purse makeChange(double amt) {
+        // Create a new purse instance
+        Purse purse = new Purse();
+
+        // Tolerance for small amounts
+        double tolerance = 0.005;
+
+        // Checks if amt is within the thresh-hold value of .009 - 0.001 and if so, return 1 penny and set amt to 0
+        if (amt < 0.01 && amt >= tolerance) {
+            Purse.Denomination penny = purse.getAllDenomination().getLast();
+            purse.add(penny, 1);
+            amt = 0;
+        }
+
+        // Iterates through denomination list, calculating minimum number of bills / coins that can fit inside the amt
+        for (Purse.Denomination d: purse.getAllDenomination()) {
+            int count = 0;
+
+            /*
+             Checks if current denomination can be used (Starting from biggest denomination to smallest)
+             if so calculate how many can fit into amt and add to purse.
+             */
+            if (d.amount() <= amt && amt >= tolerance) {
+                do {
+                    amt -= d.amount();
+                    count++;
+                } while(d.amount() <= amt && amt >= tolerance);
+
+                // Add denomination and number of bills / coins to the purse
+                purse.add(d, count);
+            }
+        }
+
+        // Clean up small value in amt bc I dunno tbh.. sounds right.
+        amt = 0;
+
+        // Return the purse object
+        return purse;
+    }
+
+    /*
+      Takes an amount inputted by user, finds the lowest number of bills to pay the amount and
+      puts it into a purse, outputting the results
+     */
+    public static void main(String[] args) {
+
+        // Output prompt
+        System.out.print("Please enter the amount you would like to add: ");
+
+        // Gets user input for variable amt
+        Scanner sc = new Scanner(System.in);
+        double amt = sc.nextDouble();
+
+        // Calculate results of makeChange, returning the Purse object
+        Purse purse = makeChange(amt);
+
+        // Output the purse object
+        System.out.print(makeChange(amt));
+
+    }
+}
